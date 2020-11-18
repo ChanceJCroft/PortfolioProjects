f0fec4c7c0d558c385480315fea8f8f5acb85833
Chance Croft <66639666+ChanceJCroft@users.noreply.github.com>
2020-11-17 17:06:42 -0800

    Add files via upload
    Finished basic CRUD functionality

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -4,9 +4,10 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 
 //connection to MongoDB
-mongoose.connect('mongodb://localhost:27017/campgrounds', {
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -23,10 +24,57 @@ db.once("open", () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+//requirements for express and method-overried
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
-//basic landing page
+//basic landing page - test
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+//GET the form for creating a new campground in new.ejs
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+//post the form for the new campground, save it, and redirect back to that campground show page
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}) 
+
+//show information for specific campground - passing in that campground information to EJS
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+})
+
+//edit a specific campground
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+})
+
+//finds the campground by the ID and adds the campground information from the req.body
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+//delete a campground
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
+
+//index showing all campgrounds
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
 })
 
 //request to make a new campground
